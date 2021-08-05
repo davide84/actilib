cb3ea116c8417ce97c819956c14121c91fb7d77b
Davide Cester <Davide.Cester@usz.ch>
2021-08-05 17:10:55 +0200

    MOD: defining ROIs, calculating average HU of slices

--- tests/test_nps.py
+++ tests/test_nps.py
@@ -1,6 +1,12 @@
 import math
-
 import numpy as np
+import cv2 as cv
+from pydicom import dcmread
+from pydicom.pixel_data_handlers.util import apply_modality_lut
+
+NPS_ROD_DIAMETER_MM = 15
+NPS_ROI_DIAMETER_MM = 30
+NPS_ROI_DISTCENT_MM = 10 + (NPS_ROD_DIAMETER_MM + NPS_ROI_DIAMETER_MM) / 2  # distance from center
 
 
 class PixelROI:
@@ -12,10 +18,14 @@ class PixelROI:
         self._center_y = center_y
         self._size = size
         print('Building ROI with center in ({},{}) and size {}'.format(center_x, center_y, size))
-        self._top = int(self._center_x - self._size / 2 + 0.5)
-        self._bottom = int(self._center_x + self._size / 2 + 0.5)
-        self._left = int(self._center_y - self._size / 2 + 0.5)
-        self._right = int(self._center_y + self._size / 2 + 0.5)
+        self._left = int(self._center_x - self._size / 2 + 0.5)
+        self._right = int(self._center_x + self._size / 2 + 0.5)
+        self._top = int(self._center_y - self._size / 2 + 0.5)
+        self._bottom = int(self._center_y + self._size / 2 + 0.5)
+        print('    pixel indexes:', self.yx_indexes())
+
+    def size(self):
+        return self._size
 
     def edge_t(self):
         return self._top
@@ -28,6 +38,9 @@ class PixelROI:
 
     def edge_r(self):
         return self._right
+
+    def yx_indexes(self):
+        return [self._top, self._bottom, self._left, self._right]
 
     def square_mask(self, image_size_x, image_size_y=None):
         """"
@@ -50,18 +63,71 @@ def rad_from_deg(deg):
 
 def create_circle_of_rois(num_rois, roi_size_px, distance_from_center_px,
                           circle_center_x_px=0, circle_center_y_px=0, angle_offset_deg=0):
-    angle_offset_rad = rad_from_deg(angle_offset_deg - 90)  # so that it starts from 12 o' clock
+    angle_offset_rad = rad_from_deg(angle_offset_deg)  # angle 0 is at 3 o' clock
     angle_spacing_rad = 2 * math.pi / num_rois
-    angles_rad = [angle_offset_rad + i * angle_spacing_rad for i in range(num_rois)]
+    angles_rad = [angle_offset_rad + r * angle_spacing_rad for r in range(num_rois)]
     return [PixelROI(circle_center_x_px + distance_from_center_px * math.cos(angle),
                      circle_center_y_px + distance_from_center_px * math.sin(angle),
                      roi_size_px) for angle in angles_rad]
 
 
-if __name__ == '__main__':
-    rois = create_circle_of_rois(8, 4, 8, 10, 10)
-    mask = np.zeros((20, 20))
-    for roi in rois:
-        mask += roi.square_mask(20)
-    print(mask)
+def find_phantom_center_and_size(numpy_array):
+    ret, thr = cv.threshold(numpy_array.astype(np.uint8), 100, 255, 0)
+    contours, hierarchy = cv.findContours(thr, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    max_r = 0
+    [center_x, center_y] = [0, 0]
+    for c in contours:
+        [x, y], r = cv.minEnclosingCircle(c)
+        if r > max_r:
+            max_r = r
+            [center_x, center_y] = [x, y]
+    return [center_x, center_y], max_r
 
+
+if __name__ == '__main__':
+    #
+    # read the images
+    #
+    images = []
+    for i in range(16):
+        filename = '/home/cester/git/actilib/src/actilib/resources/dicom/nps/{:04d}_nps.dcm'.format(161-i)
+        dicom_data = dcmread(filename)
+        images.append(apply_modality_lut(dicom_data.pixel_array, dicom_data))
+    pixel_size_xy_mm = np.array(dicom_data.PixelSpacing)
+    image_size_xy_px = np.array([len(images[0]), len(images[0][0])])
+    image_center_xy_px, section_radius_px = find_phantom_center_and_size(images[0])
+
+    # print('Diameter [mm]:', section_radius_px * 2 * pixel_size_xy_mm)
+
+    # print(image_size_xy_px, image_center_xy_px, pixel_size_xy_mm)
+
+    #
+    # create the ROIs
+    #
+    roi_diameter_px = NPS_ROI_DIAMETER_MM / pixel_size_xy_mm[0]
+    roi_distcent_px = NPS_ROI_DISTCENT_MM / pixel_size_xy_mm[0]
+    rois = create_circle_of_rois(8, roi_diameter_px, roi_distcent_px, image_center_xy_px[0], image_center_xy_px[1])
+
+    #
+    # loop on the ROIs, extract the pixels and calculate the average HU
+    #
+    hu_values = []
+    for i_roi, roi in enumerate(rois):
+        [y1, y2, x1, x2] = roi.yx_indexes()
+        for image in images:
+            roi_image = image[y1:y2, x1:x2]  # (!) in "numpy images" the 1st coordinate is y
+            hu = np.mean(roi_image)
+            hu_values.append(hu)
+    print(np.mean(hu_values))
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.imshow(images[0], cmap=plt.cm.bone, vmin=-255, vmax=255)
+    import matplotlib.patches as patches
+    for roi in rois:
+        x = roi.edge_l()
+        y = roi.edge_t()
+        rect = patches.Rectangle((x, y), roi.size(), roi.size(), linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    plt.show()

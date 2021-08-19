8f98a74dbd075d2feeb4218d7070aa529a6f1ac6
Davide Cester <Davide.Cester@usz.ch>
2021-08-19 18:37:12 +0200

    ADD: stub for TTF calculation (WIP: insert finding)

--- tests/test_ttf.py
+++ tests/test_ttf.py
@@ -1,10 +1,9 @@
 import numpy as np
 import os
 import pkg_resources
-import tarfile
-from pydicom import dcmread
-from pydicom.pixel_data_handlers.util import apply_modality_lut
-from actilib.helpers.geometry import find_phantom_center_and_radius
+from actilib.helpers.geometry import find_phantom_center_and_radius, find_circles
+from actilib.helpers.display import plot_image_with_rois
+from actilib.helpers.general import load_images_from_tar
 from actilib.helpers.rois import create_circle_of_rois
 
 
@@ -17,24 +16,37 @@ def main():
     #
     # read the images and basic properties
     #
-    images = []
     tarpath = pkg_resources.resource_filename('actilib', os.path.join('resources', 'dicom_ttf.tar.xz'))
-    with tarfile.open(tarpath, encoding='utf-8') as file_tar:
-        for file_name in file_tar.getmembers():
-            file_dcm = file_tar.extractfile(file_name)
-            dicom_data = dcmread(file_dcm)
-            images.append(apply_modality_lut(dicom_data.pixel_array, dicom_data))
-    pixel_size_xy_mm = np.array(dicom_data.PixelSpacing)
+    images, headers = load_images_from_tar(tarpath)
+    print('Read', str(len(images)), 'images.')
+    pixel_size_xy_mm = np.array(headers[0].PixelSpacing)
     image_size_xy_px = np.array([len(images[0]), len(images[0][0])])
     image_center_xy_px, section_radius_px = find_phantom_center_and_radius(images)
 
     #
-    # create and visualize the ROIs
+    # find the inserts
     #
-    roi_diameter_px = NPS_ROI_DIAMETER_MM / pixel_size_xy_mm[0]
-    roi_distcent_px = NPS_ROI_DISTCENT_MM / pixel_size_xy_mm[0]
-    rois = create_circle_of_rois(8, roi_diameter_px, roi_distcent_px,
-                                 image_center_xy_px[0], image_center_xy_px[1])
+    circles = find_circles(images[0], 15/pixel_size_xy_mm[0], 10)
+    print(1.5, pixel_size_xy_mm[0], 15/pixel_size_xy_mm[0])
+    print(len(circles))
+    print(circles)
+
+    roi_diameter_px = (NPS_ROI_DIAMETER_MM + 5) / pixel_size_xy_mm[0]
+    roi_distcent_px = (NPS_ROI_DISTCENT_MM + 10) / pixel_size_xy_mm[0]
+    rois = create_circle_of_rois(5, roi_diameter_px, roi_distcent_px,
+                                 image_center_xy_px[0], image_center_xy_px[1],
+                                 angle_offset_deg=42, roi_shape='circular')
+
+    from actilib.helpers.display import plot_image_with_rois
+    plot_image_with_rois(images[0], headers[0], rois)
+
+    #
+    # create the ROIs using image pixel coordinates as reference system
+    #
+    # roi_diameter_px = NPS_ROI_DIAMETER_MM / pixel_size_xy_mm[0]
+    # roi_distcent_px = NPS_ROI_DISTCENT_MM / pixel_size_xy_mm[0]
+    # rois = create_circle_of_rois(8, roi_diameter_px, roi_distcent_px,
+    #                              image_center_xy_px[0], image_center_xy_px[1])
 
     #
     # loop on the ROIs, extract the pixels and calculate the ROI values

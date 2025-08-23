afb2f2cb4a9edbb1cb699bd08fff2e525fc094c7
cester <21276516+davide84@users.noreply.github.com>
2025-08-23 15:51:52 +0200

    MOD: test_gnl.py with algorithm comparison

--- tests/test_gnl.py
+++ tests/test_gnl.py
@@ -7,26 +7,30 @@ from actilib.analysis.gnl import calculate_gnl
 # gnl = calculate_gnl(image, SegMats.LUNGS, hu_ranges={SegMats.LUNGS:[-700,-200]})
 # print(gnl)
 
-image = load_image_from_path('/home/cester/Data/CBCT Spine/Rearranged/CBCT_17x47e_1mm/Bestquality_100kVp_75mA_11.52s_4.81mGy/0400.dcm')
-gnl1, gnl2, pixels, segm, figure = calculate_gnl(image, SegMats.SOFT_TISSUE, return_plot_data=True, mask_rois=[(0,20,0,20,-1000)], no_noise_value=0)
-print(gnl1, gnl2)
+image = load_image_from_path('/home/cester/Data/Paper - HAND/Rearranged/EICT - Hand - MPR - 1.0mm/00000160.dcm')
 
 import matplotlib.pyplot as plt
-import numpy as np
+from time import time
 
-mask = np.where(segm == SegMats.SOFT_TISSUE.value, segm, np.zeros_like(segm))
-print(np.unique(segm))
-print(np.unique(mask))
-fig, axs = plt.subplots(1, 4, figsize=(12, 7))
-axs[0].imshow(pixels, cmap='gray', interpolation='nearest')
-axs[1].imshow(mask, cmap='gray', interpolation='nearest')
-# axs[1].imshow(mask, cmap='tab10')
-axs[2].imshow(figure, cmap='nipy_spectral', interpolation='nearest')
-img = axs[3].imshow(figure, cmap='nipy_spectral', interpolation='nearest')
-plt.colorbar(img, orientation='horizontal', shrink=0.75)
-for i in range(3):
-    axs[i].set_xticks([])
-    axs[i].set_yticks([])
-plt.tight_layout()
-plt.show()
-
+fig, axs = plt.subplots(3, 2, figsize=(8, 8))
+gnlmap = [None, None, None]
+for a, algorithm in enumerate(['generic_filter', 'convolution', 'sliding_window_view']):
+    time_start = time()
+    gnl1, gnl2, pixels, segmap, gnlmap[a] = calculate_gnl(dicom_images=image,
+                                                          tissues=SegMats.AIR,
+                                                          return_plot_data=True,
+                                                          # mask_rois=[(0,20,0,20,-1000)],
+                                                          algorithm=algorithm)
+    title = '{} ({:.1f} s) GNL = {}'.format(algorithm, time() - time_start, gnl1)
+    print(title)
+    axs[a, 0].set_title(title)
+    img = axs[a, 0].imshow(gnlmap[a], cmap='nipy_spectral', interpolation='nearest')
+    plt.colorbar(img, orientation='horizontal', shrink=0.75)
+    img = axs[a, 1].imshow(gnlmap[a]-gnlmap[0], cmap='nipy_spectral', interpolation='nearest')
+    plt.colorbar(img, orientation='horizontal', shrink=0.75)
+    axs[a, 0].set_xticks([])
+    axs[a, 0].set_yticks([])
+    axs[a, 1].set_xticks([])
+    axs[a, 1].set_yticks([])
+    plt.tight_layout()
+    plt.savefig('compare.png')

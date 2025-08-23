b631fa5e27bf405a48410bc29b3da08e31337b4c
cester <21276516+davide84@users.noreply.github.com>
2025-08-23 15:51:31 +0200

    MOD: implemented three similar algorithms for local STD

--- src/actilib/analysis/gnl.py
+++ src/actilib/analysis/gnl.py
@@ -1,5 +1,6 @@
 import numpy as np
-# from scipy.ndimage import generic_filter
+from scipy.ndimage import generic_filter
+from scipy.signal import convolve2d
 from numpy.lib.stride_tricks import sliding_window_view
 from actilib.analysis.segmentation import SegMats, get_default_segmentation_thresholds, segment_with_thresholds
 
@@ -11,9 +12,35 @@ This function takes a single DICOM image (including headers) as input and calcul
 """
 
 
-def calculate_gnl(dicom_images, tissues=SegMats.SOFT_TISSUE, kernel_size_mm=6,
+def get_histogram_mode(data):
+    histo_max = int(np.nanmax(data) + 1)
+    histogram, bin_edges = np.histogram(data, bins=histo_max, range=(1, histo_max))
+    return np.argmax(histogram)  # bin size = 1 -> bin index = bin upper edge = x value
+
+
+def calculate_local_std(img, kernel_size, algorithm):
+    kernel_shape = (kernel_size, kernel_size)
+    if 'generic_filter' == algorithm:  # probably the most accurate, but slow
+        img_std = generic_filter(img, np.std, size=kernel_size)
+    elif 'convolution' == algorithm:  # almost the fastest and handles the boundaries
+        kernel = np.ones(kernel_shape) / (kernel_size ** 2)
+        mean_of_sq = convolve2d(img ** 2, kernel, mode='same', boundary='symm')
+        sq_of_mean = convolve2d(img, kernel, mode='same', boundary='symm') ** 2
+        img_std = np.sqrt(mean_of_sq - sq_of_mean)
+    elif 'sliding_window_view' == algorithm:  # 30% fastest as convolution, but must be padded
+        margin = int((kernel_size - 1) / 2)
+        img_pad = np.full((margin + margin + img.shape[0], margin + margin + img.shape[1]), np.nan)
+        img_pad[margin:-margin, margin:-margin] = img
+        img_std = sliding_window_view(img_pad, window_shape=kernel_shape).std(axis=(2, 3))
+    else:
+        raise NotImplementedError('algorithm "' + algorithm + '"')
+    return img_std
+
+
+def calculate_gnl(dicom_images, tissues=SegMats.SOFT_TISSUE, kernel_radius_mm=3,
                   hu_ranges=get_default_segmentation_thresholds(),
-                  no_noise_value=np.nan, return_plot_data=False, mask_rois=None):
+                  return_plot_data=False, mask_rois=None,
+                  algorithm='convolution'):
     # input preparation
     if not isinstance(dicom_images, list):
         dicom_images = [dicom_images]
@@ -31,22 +58,18 @@ def calculate_gnl(dicom_images, tissues=SegMats.SOFT_TISSUE, kernel_size_mm=6,
                 pixels[roi[0]:roi[1], roi[2]:roi[3]] = roi[4]
         # 1. threshold-based segmentation
         segmap = segment_with_thresholds(pixels, hu_ranges)
-        # 2. calculation of local SD
-        kernel_size_px = np.round(kernel_size_mm / dicom_image['header'].PixelSpacing[0]).astype(int)
+        # 2. calculation of local SD - kernel has always an odd number of pixels
+        kernel_size_px = 1 + np.round(2 * kernel_radius_mm / dicom_image['header'].PixelSpacing[0]).astype(int)
         if [None] != tissues:
-            gnlmap = np.full(segmap.shape, no_noise_value)
+            gnlmap = np.zeros(segmap.shape)
             for tissue in tissues:
-                img_segm = np.where(segmap == tissue.value, pixels, np.nan)
-                # two different implementations, sliding_window_view is >6 times faster
-                # img_gnl = generic_filter(img_segm, np.nanstd, size=kernel_size_px)
-                img_gnl = np.nanstd(sliding_window_view(img_segm, window_shape=(kernel_size_px, kernel_size_px)))
+                img_segm = np.where(segmap == tissue.value, pixels, 0)
+                img_gnl = calculate_local_std(img_segm, kernel_size_px, algorithm)
                 gnlmap = np.where(segmap == tissue.value, img_gnl, gnlmap)
         else:
-            gnlmap = np.nanstd(sliding_window_view(pixels, window_shape=(kernel_size_px, kernel_size_px)))
+            gnlmap = calculate_local_std(img_segm, kernel_size_px, algorithm)
         # 3. histogram of local SD and mode
-        histo_max = int(np.nanmax(gnlmap) + 1)
-        histogram, bin_edges = np.histogram(gnlmap, bins=histo_max, range=(1, histo_max))
-        gnls.append(np.argmax(histogram))  # bin size = 1 -> bin index = bin upper edge = x value
+        gnls.append(get_histogram_mode(gnlmap))
         if return_plot_data:
             return np.mean(gnls), np.std(gnls), pixels, segmap, gnlmap
     return np.mean(gnls), np.std(gnls)
